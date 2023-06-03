e7eb5ca95b6629a4044621f8494234e9f7c93e8f
Salaheddin AbuEin <sabuein@gmail.com>
2023-06-03 02:25:21 +0100

    Adding different routes

---
+++ api/modules/routes.mjs
@@ -0,0 +1,7 @@
+"use strict";
+
+import { authors } from "../routes/authors.mjs";
+import { books } from "../routes/books.mjs";
+import { users } from "../routes/users.mjs";
+
+export { authors, books, users };
