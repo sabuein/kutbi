6cbcd72b731d98c2e6acf0218a164cc25d143b13
Salaheddin AbuEin <sabuein@gmail.com>
2023-06-11 02:43:22 +0100

    Updating SQL

--- api/data/tables.sql
+++ api/data/tables.sql
@@ -1,51 +1,64 @@
 /*
-
-SHOW DATABASES;
-CREATE DATABASE kutbi_27may;
-
-MariaDB [kutbi_27may]> SHOW TABLES;
-+-----------------------+
-| Tables_in_kutbi_27may |
-+-----------------------+
-| Authors               |
-| Book_Authors          |
-| Book_Genres           |
-| Book_Images           |
-| Books                 |
-| Genres                |
-| Publishers            |
-| Reviews               |
-| User_Passwords        |
-| Users                 |
-+-----------------------+
-
-CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON database_name.* TO 'username'@'localhost';
-
-SHOW VARIABLES LIKE 'max_connections';
-SHOW STATUS WHERE `variable_name` = 'Threads_connected';
-
-*/
-
+ 
+ SHOW DATABASES;
+ CREATE DATABASE kutbi_27may;
+ 
+ MariaDB [kutbi_27may]> SHOW TABLES;
+ +-----------------------+
+ | Tables_in_kutbi_27may |
+ +-----------------------+
+ | Authors               |
+ | BookAuthors           |
+ | BookGenres            |
+ | BookImages            |
+ | Books                 |
+ | Genres                |
+ | Publishers            |
+ | Reviews               |
+ | UserPasswords         |
+ | Users                 |
+ +-----------------------+
+ 
+ CREATE USER 'username'@'localhost' IDENTIFIED BY 'password';
+ GRANT ALL PRIVILEGES ON database_name.* TO 'username'@'localhost';
+ 
+ SHOW VARIABLES LIKE 'max_connections';
+ SHOW STATUS WHERE `variable_name` = 'Threads_connected';
+ 
+ */
 USE kutbi_27may;
 
 -- Drop all tables, if exists
 DROP TABLE IF EXISTS Reviews;
-DROP TABLE IF EXISTS User_Passwords;
+
+DROP TABLE IF EXISTS UserPermissions;
+
+DROP TABLE IF EXISTS UserRoles;
+
+DROP TABLE IF EXISTS UserPasswords;
+
 DROP TABLE IF EXISTS Users;
-DROP TABLE IF EXISTS Book_Genres;
+
+DROP TABLE IF EXISTS BookGenres;
+
 DROP TABLE IF EXISTS Genres;
-DROP TABLE IF EXISTS Book_Authors;
-DROP TABLE IF EXISTS Book_Images;
+
+DROP TABLE IF EXISTS BookAuthors;
+
+DROP TABLE IF EXISTS BookImages;
+
 DROP TABLE IF EXISTS Books;
+
 DROP TABLE IF EXISTS Publishers;
+
 DROP TABLE IF EXISTS Authors;
 
 -- Create Authors table
 CREATE TABLE IF NOT EXISTS Authors (
-    author_id INT AUTO_INCREMENT PRIMARY KEY,
-    fname VARCHAR(100),
-    lname VARCHAR(100),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    fname VARCHAR(255) NOT NULL,
+    lname VARCHAR(255) NOT NULL,
+    bio TEXT,
     dob DATE,
     lang VARCHAR(50),
     tel VARCHAR(20),
@@ -57,18 +70,17 @@ CREATE TABLE IF NOT EXISTS Authors (
     instagram VARCHAR(100),
     youtube VARCHAR(100),
     website VARCHAR(100),
-    bio TEXT,
-    photo VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    edited_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    photoUrl VARCHAR(255),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    editedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt TIMESTAMP
 );
 
 -- Create Publishers table
 CREATE TABLE IF NOT EXISTS Publishers (
-    publisher_id INT AUTO_INCREMENT PRIMARY KEY,
-    fname VARCHAR(100),
-    lname VARCHAR(100),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    company VARCHAR(255),
+    imprint VARCHAR(255),
     bio TEXT,
     country VARCHAR(100),
     phone VARCHAR(20),
@@ -79,111 +91,134 @@ CREATE TABLE IF NOT EXISTS Publishers (
     instagram VARCHAR(100),
     youtube VARCHAR(100),
     website VARCHAR(100),
-    photo_url VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    edited_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    photoUrl VARCHAR(255),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    editedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt TIMESTAMP
 );
 
 -- Create Books table
 CREATE TABLE IF NOT EXISTS Books (
-    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(200),
-    publication_date DATE,
-    publisher_id INT,
+    publicationDate DATE,
+    publisherId INT,
     lang VARCHAR(50),
     paperback BOOLEAN,
-    isbn_10 VARCHAR(20),
-    isbn_13 VARCHAR(20),
+    isbn10 VARCHAR(20),
+    isbn13 VARCHAR(20),
     dimensions VARCHAR(100),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    edited_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP,
-    FOREIGN KEY (publisher_id) REFERENCES Publishers(publisher_id)
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    editedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt TIMESTAMP,
+    FOREIGN KEY (publisherId) REFERENCES Publishers(id)
 );
 
--- Create Book_Images table (Many-to-Many Mapping)
-CREATE TABLE IF NOT EXISTS Book_Images (
+-- Create BookImages table (Many-to-Many Mapping)
+CREATE TABLE IF NOT EXISTS BookImages (
     id INT PRIMARY KEY,
-    book_id INT,
-    photo_url VARCHAR(255),
-    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+    bookId INT,
+    photoUrl VARCHAR(255),
+    FOREIGN KEY (bookId) REFERENCES Books(id)
 );
 
--- Create Book_Authors table (Many-to-Many Mapping)
-CREATE TABLE IF NOT EXISTS Book_Authors (
-    book_id INT,
-    author_id INT,
-    FOREIGN KEY (book_id) REFERENCES Books(book_id),
-    FOREIGN KEY (author_id) REFERENCES Authors(author_id),
-    PRIMARY KEY (book_id, author_id)
+-- Create BookAuthors table (Many-to-Many Mapping)
+CREATE TABLE IF NOT EXISTS BookAuthors (
+    bookId INT,
+    authorId INT,
+    FOREIGN KEY (bookId) REFERENCES Books(id),
+    FOREIGN KEY (authorId) REFERENCES Authors(id),
+    PRIMARY KEY (bookId, authorId)
 );
 
 -- Create Genres table
 CREATE TABLE IF NOT EXISTS Genres (
-    genre_id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    edited_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category VARCHAR(100),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    editedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt TIMESTAMP
 );
 
--- Create Book_Genres table (Many-to-Many Mapping)
-CREATE TABLE IF NOT EXISTS Book_Genres (
-    book_id INT,
-    genre_id INT,
-    FOREIGN KEY (book_id) REFERENCES Books(book_id),
-    FOREIGN KEY (genre_id) REFERENCES Genres(genre_id),
-    PRIMARY KEY (book_id, genre_id)
+-- Create BookGenres table (Many-to-Many Mapping)
+CREATE TABLE IF NOT EXISTS BookGenres (
+    bookId INT,
+    genreId INT,
+    FOREIGN KEY (bookId) REFERENCES Books(id),
+    FOREIGN KEY (genreId) REFERENCES Genres(id),
+    PRIMARY KEY (bookId, genreId)
 );
 
 -- Create Users table
 CREATE TABLE IF NOT EXISTS Users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100),
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    uuid VARCHAR(36) NOT NULL DEFAULT LOWER(SYS_GUID()),
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
     fname VARCHAR(100),
     lname VARCHAR(100),
     dob DATE,
     bio TEXT,
     country VARCHAR(100),
     phone VARCHAR(20),
-    email VARCHAR(100),
     github VARCHAR(100),
     twitter VARCHAR(100),
     facebook VARCHAR(100),
     instagram VARCHAR(100),
     youtube VARCHAR(100),
     website VARCHAR(100),
-    photo_url VARCHAR(255),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    edited_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP
+    photoUrl VARCHAR(255),
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    editedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt TIMESTAMP
 );
 
--- Create User_Passwords table
-CREATE TABLE IF NOT EXISTS User_Passwords (
-    user_id INT,
-    password_hash VARCHAR(100),
-    salt VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    edited_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES Users(user_id),
-    PRIMARY KEY (user_id)
+-- Create UserPasswords table
+CREATE TABLE IF NOT EXISTS UserPasswords (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    salt VARCHAR(50) NOT NULL,
+    passwordHash VARCHAR(255) NOT NULL,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    editedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt TIMESTAMP,
+    FOREIGN KEY (userId) REFERENCES Users(id)
+);
+
+-- Create UserRoles table
+CREATE TABLE IF NOT EXISTS UserRoles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    roles VARCHAR(255) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES Users(id)
+);
+
+-- Create UserPermissions table
+CREATE TABLE IF NOT EXISTS UserPermissions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT,
+    permissions VARCHAR(255) NOT NULL,
+    FOREIGN KEY (userId) REFERENCES Users(id)
 );
 
 -- Create Reviews table
 CREATE TABLE IF NOT EXISTS Reviews (
-    review_id INT AUTO_INCREMENT PRIMARY KEY,
-    book_id INT,
-    user_id INT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    bookId INT,
+    userId INT,
     rating INT,
-    review_text TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    edited_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP,
-    FOREIGN KEY (book_id) REFERENCES Books(book_id),
-    FOREIGN KEY (user_id) REFERENCES Users(user_id)
+    review TEXT,
+    createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    editedAt TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deletedAt TIMESTAMP,
+    FOREIGN KEY (bookId) REFERENCES Books(id),
+    FOREIGN KEY (userId) REFERENCES Users(id)
 );
 
 SHOW TABLES;
+
+/*
+ sudo systemctl stop mariadb
+ sudo apt-get remove mariadb-server
+ https://www.liquidweb.com/kb/upgrade-mariadb-ubuntu-18-04/
+ */
