afe2b97270809d4c92a06c266985d2b69a3f9aec
Salaheddin AbuEin <sabuein@gmail.com>
2023-06-21 03:33:37 +0100

    Updating accounts logic

--- apis/v1/routes/accounts.mjs
+++ apis/v1/routes/accounts.mjs
@@ -1,12 +1,13 @@
 "use strict";
 
 import express from "express";
-import { login, recover, register, validateAuthHeader, validateAuthCookie, clearAuthTokens, clearAuthCookies } from "../modules/auth.mjs";
+import { login, recover, register, setupAuth, validateAuthHeader, validateAuthCookie, clearAuthTokens, clearAuthCookies } from "../modules/auth.mjs";
 import { roles, checkPermission } from "../modules/roles.mjs";
 
 const accounts = express.Router();
 
 // Browse, Read, Edit, Add, Copy, Delete
+const types = ["subscriber", "user"];
 
 accounts
     .route("/")
@@ -17,25 +18,17 @@ accounts
 
 accounts
     .route("/signup")
-    .post(register, async (req, res) => {
-        const types = ["subscriber", "user"];
+    .post(register, setupAuth, async (req, res) => {
         if (!types.includes(req.account.type)) return res.status(500).json({ error: "Server error" });
-        const account = req.account;
-        setHeaders(res, { accessToken: account.accessToken, refreshToken: account.refreshToken });
-        setCookies(res, { accessToken: account.accessToken, refreshToken: account.refreshToken });
-        return res.status(201).json(account);
+        return res.status(201).json(req.account);
     });
 
 accounts
     .route("/signin")
-    .post(login, (req, res) => {
-        const types = ["subscriber", "user"];
+    .post(login, setupAuth, (req, res) => {
         if (!types.includes(req.account.type)) return res.status(500).json({ error: "We couldn't locate your Kutbi account" });
-        const account = req.account;
-        setHeaders(res, { accessToken: account.accessToken, refreshToken: account.refreshToken });
-        setCookies(res, { accessToken: account.accessToken, refreshToken: account.refreshToken });
-        console.log(`Details: A Kutbi account of type ${account.type} (#${account.guid}) have successfully retrieved and logged in.\r\n`);
-        return res.status(200).json(account);
+        console.log(`Details: A Kutbi account of type ${req.account.type} (#${req.account.guid}) have successfully retrieved and logged in.\r\n`);
+        return res.status(200).json(req.account);
     });
 
 accounts
@@ -50,28 +43,6 @@ accounts
         console.log("TODO: Delete token from database");
         return res.status(204).json({ message: "Successfully deleted refresh token" });
     });
-
-const setCookies = (response, account) => {
-    response.cookie("accessToken", account.accessToken, {
-        httpOnly: false,
-        secure: true,
-        maxAge: 3600000, // One hour in milliseconds
-        signed: false
-    });
-
-    response.cookie("refreshToken", account.refreshToken, {
-        httpOnly: false,
-        secure: true,
-        maxAge: 2629800000, // One month in milliseconds
-        signed: false
-    });
-};
-
-const setHeaders = (response, account) => {
-    response.set("Authorization", `Bearer ${account.accessToken}`);
-    // response.set("Cache-Control", ``);
-    // response.set("Content-Security-Policy", ``);
-};
 
 export { accounts };
 
