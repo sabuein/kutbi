763b301dd8e709db140c0abc69a7ae6f3971761c
Salaheddin AbuEin <sabuein@gmail.com>
2023-09-09 02:19:59 +0100

    Updating logic

--- pwa/serviceWorker.js
+++ pwa/serviceWorker.js
@@ -1,6 +1,9 @@
 // Service worker: A client side proxy written in JavaScript
 // https://www.w3.org/TR/service-workers/
 
+const cacheVersion = 1;
+const cacheName = `kutbi-${cacheVersion}`;
+
 const patterns = {
     scripts: "\.(js|mjs)$",
     stylesheets: "\.css$",
@@ -9,70 +12,63 @@ const patterns = {
     json: "\.json$"
 };
 
-const precache = [
-    "./",
-    "./authors.html",
-    "./change-password.html",
-    "./cookies.html",
-    "./faq.html",
-    "./features.html",
-    "./index.html",
-    "./kutbi.webmanifest",
-    "./login.html",
-    "./mobile.html",
-    "./purpose.html",
-    "./recover.html",
-    "./register.html",
-    "./sitemap.xml",
-    "./wishlist.html",
-    "./worker.js",
-    "./assets/",
-    "./assets/css/",
-    "./assets/css/layout.css",
-    "./assets/fonts/",
-    "./assets/images/",
-    "./assets/js/",
-    "./assets/js/app.mjs",
-    "./assets/js/modules/apis.mjs",
-    "./assets/js/modules/auth.mjs",
-    "./assets/js/modules/data.mjs",
-    "./assets/js/modules/helpers.mjs",
-    "./assets/js/modules/interface.mjs",
-    "./assets/js/modules/monitor.mjs",
-    "./assets/js/modules/objects.mjs",
-    "./assets/js/modules/requests.mjs"
+const cacheList = [
+    "/pwa/",
+    "/pwa/authors.html",
+    "/pwa/change-password.html",
+    "/pwa/cookies.html",
+    "/pwa/faq.html",
+    "/pwa/features.html",
+    "/pwa/index.html",
+    "/pwa/kutbi.webmanifest",
+    "/pwa/login.html",
+    "/pwa/mobile.html",
+    "/pwa/purpose.html",
+    "/pwa/recover.html",
+    "/pwa/register.html",
+    "/pwa/sitemap.xml",
+    "/pwa/wishlist.html",
+    "/pwa/assets/css/layout.css",
+    "/pwa/assets/fonts/CircularXXWeb-Book.woff2",
+    "/pwa/assets/js/app.js",
+    "/pwa/assets/js/modules/apis.mjs",
+    "/pwa/assets/js/modules/auth.mjs",
+    "/pwa/assets/js/modules/data.mjs",
+    "/pwa/assets/js/modules/helpers.mjs",
+    "/pwa/assets/js/modules/interface.mjs",
+    "/pwa/assets/js/modules/monitor.mjs",
+    "/pwa/assets/js/modules/objects.mjs",
+    "/pwa/assets/js/modules/requests.mjs",
+    "/pwa/assets/images/add.png",
+    "/pwa/assets/images/apple-touch-icon.png",
+    "/pwa/assets/images/favicon-16x16.png",
+    "/pwa/assets/images/favicon-32x32.png",
+    "/pwa/assets/images/favicon.ico",
+    "/pwa/assets/images/screenshot1.png",
+    "/pwa/assets/images/screenshot2.jpg",
+    "/pwa/assets/images/android/android-launchericon-144-144.png",
+    "/pwa/assets/images/svg/books.svg"
 ];
 
 self.addEventListener("install", (event) => {
     try {
         // Setting up caches, offline assets, populating an IndexedDB, etc.
         event.waitUntil(
-            caches.open("v1")
-                .then(cache => cache.addAll(precache))
+            caches.open(cacheName)
+                .then(cache => cache.addAll(cacheList))
                 .then(self.skipWaiting())
         );
-
-        const worker = new Worker("worker.js");
-        console.log("A web worker is here!");
-        worker.onmessage = (event) => console.log(event.data);
-        worker.postMessage([5, 3]);
     } catch (error) {
         console.error(error);
     }
 });
 
 
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", async (event) => {
     // Clean up resources used in previous versions of the service worker
-    const cacheAllowlist = ["v1"];
-    event.waitUntil(
-        caches.forEach((cache, cacheName) => {
-            if (!cacheAllowlist.includes(cache)) {
-                return caches.delete(cacheName);
-            }
-        }).then(() => self.clients.claim())
-        // Clients loaded in the same scope do not need to be reloaded before their fetches will go through this service worker
-    );
+    await deleteOldCaches(cacheName);
+    // Clients loaded in the same scope do not need to be reloaded before their fetches will go through this service worker
+    self.clients.claim();
 });
 
 self.addEventListener("message", (event) => {
@@ -86,7 +82,6 @@ self.addEventListener("message", (event) => {
             ports: event.ports
         };
         console.log(`The client sent me a message: ${event.data}`);
-        console.dir(properties);
         event.source.postMessage("Hi client");
     } catch (error) {
         console.error(error);
@@ -99,7 +94,6 @@ self.addEventListener("sync", (event) => {
             tag: event.tag,
             lastChance: event.lastChance
         };
-        console.log(properties);
 
         if (event.tag == "important-thing") {
             event.waitUntil(
@@ -120,23 +114,22 @@ self.addEventListener("fetch", (event) => {
     try {
         const url = new URL(event.request.url);
         console.log(`Handling fetch event for ${url}`);
-/*
-        if (patterns.scripts.test(url.pathname)) event.respondWith(cacheOnly(event.request));
-        else if (patterns.stylesheets.test(url.pathname)) event.respondWith(cacheThenNetwork(event.request));
-        else if (patterns.images.test(url.pathname)) event.respondWith(cacheThenNetwork(event.request));
-        else if (patterns.fonts.test(url.pathname)) event.respondWith(cacheOnly(event.request));
-        else if (patterns.json.test(url.pathname)) event.respondWith(cacheThenNetwork(event.request));
-        else if (url.pathname.includes("secret")) event.respondWith(Response.error());
-        else event.respondWith(Response.json({ message: "All's good!" }));*/
+        /*
+                if (patterns.scripts.test(url.pathname)) event.respondWith(cacheOnly(event.request));
+                else if (patterns.stylesheets.test(url.pathname)) event.respondWith(cacheThenNetwork(event.request));
+                else if (patterns.images.test(url.pathname)) event.respondWith(cacheThenNetwork(event.request));
+                else if (patterns.fonts.test(url.pathname)) event.respondWith(cacheOnly(event.request));
+                else if (patterns.json.test(url.pathname)) event.respondWith(cacheThenNetwork(event.request));
+                else if (url.pathname.includes("secret")) event.respondWith(Response.error());
+                else event.respondWith(Response.json({ message: "All's good!" }));*/
 
         // The string must be one of the audio, audioworklet, document, embed, font, frame, iframe, image, manifest, object, paintworklet, report, script, sharedworker, style, track, video, worker or xslt strings, or the empty string, which is the default value.
         if (
             event.request.destination === "script" ||
             event.request.destination === "style"
         ) {
-            if (url.pathname.includes(".mjs")) event.respondWith(cacheThenNetwork(event.request));
-            else event.respondWith(cacheOnly(event.request));
-        }
+            event.respondWith(cacheThenNetwork(event.request));
+        } else event.respondWith(cacheThenNetwork(event.request));
     } catch (error) {
         console.error(error);
     }
@@ -146,7 +139,6 @@ self.addEventListener("push", (event) => {
     // Returns a reference to a PushMessageData object
     try {
         const message = event.data.json();
-
         switch (message.type) {
             case "init":
                 doInit();
@@ -155,8 +147,6 @@ self.addEventListener("push", (event) => {
                 doShutdown();
                 break;
         }
-
-        console.dir(event);
     } catch (error) {
         console.error(error);
     }
@@ -173,30 +163,48 @@ const doShutdown = () => {
 // Cache falling back to network
 const cacheThenNetwork = async (request) => {
     try {
-        const cachedResponse = await caches.match(request);
-        if (cachedResponse) {
+        const cachedResponse = await caches.match(request, { cacheName: cacheName });
+        if (!!cachedResponse) {
             console.log("Found response in cache:", cachedResponse);
             return cachedResponse;
+        } else {
+            console.log("Falling back to network");
+            console.log(request);
+            const fetchedResponse = await fetch(request);
+            if (!!fetchedResponse) {
+                const responseClone = fetchedResponse.clone();
+                await caches.open(cacheName).put(request, responseClone);
+                return await cacheOnly(request) || await fetch(request);
+            }
         }
-        console.log("Falling back to network");
-        return fetch(request);
     } catch (error) {
         console.error(error);
+        // () => caches.match("/pwa/assets/images/error.jpg");
     }
 };
 
 // Cache only
 const cacheOnly = async (request) => {
     try {
-        const cachedResponse = await caches.match(request);
-        if (cachedResponse) {
+        const cachedResponse = await caches.match(request, { cacheName: cacheName });
+        if (!!cachedResponse) {
             console.log("Found response in cache:", cachedResponse);
             return cachedResponse;
-        } else {
-            return Response.error();
-        }
+        } // else Response.error();
     } catch (error) {
         console.error(error);
+        // () => caches.match("/pwa/assets/images/error.jpg");
+    }
+};
+
+const deleteOldCaches = async (currentCache) => {
+    const keys = await caches.keys();
+    for (const key of keys) {
+        const isOurCache = key.startsWith("kutbi-");
+        if (currentCache === key || !isOurCache) {
+            continue;
+        }
+        caches.delete(key);
     }
 };
 
--- pwa/worker.js
+++ pwa/worker.js
@@ -1,6 +1,16 @@
 "use strict";
 
-self.onmessage = (event) => {
+importScripts(); /* imports nothing */
+
+self.addEventListener("message", (event) => {
+    console.log("Message received from main script");
     const [a, b] = event.data;
+    console.log("Posting message back to main script");
     self.postMessage(a + b);
-};
+});
+
+self.addEventListener("error", (event) => {
+    // The error event has three fields: message, filename, and lineno.
+    event.preventDefault();
+    console.error(event.message);
+});
